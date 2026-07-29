a145f6ae2dfb40bb9c9b68c71152ecf5e49f46bb
EUNSOLY <dmsthf9596@gmail.com>
2026-07-29 22:50:02 +0900

    코드 개선 : BaseEntity 클래스 구체 클래스 -> 추상클래스로 변경
    - 단순히 추상메서드가 없으니까 추상클래스가 불필요하다고 생각했었는데 추상클래스를 사용함으로써 혹시 모를 외부에서의 부모 클래스 호출을 방지할수있다.
    - 실수를 방지하는 코드가 좋은코드다!!! 명심

--- src/main/java/com/sprint/mission/discodeit/entity/BaseEntity.java
+++ src/main/java/com/sprint/mission/discodeit/entity/BaseEntity.java
@@ -6,7 +6,7 @@ import java.io.Serializable;
 import java.util.UUID;
 
 //@Getter
-public class BaseEntity implements Serializable {
+public abstract class BaseEntity implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
     private final UUID id;
