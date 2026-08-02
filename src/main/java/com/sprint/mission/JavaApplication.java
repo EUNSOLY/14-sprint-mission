08b0ee3bf3b2a944b72fd48b7d6246d186df4666
EUNSOLY <dmsthf9596@gmail.com>
2026-08-02 22:54:55 +0900

    init : 스프린트 미션 3 불필요 파일 초기화

--- src/main/java/com/sprint/mission/JavaApplication.java
+++ src/main/java/com/sprint/mission/JavaApplication.java
@@ -4,146 +4,52 @@ import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.entity.UserStatus;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.repository.file.FileChannelRepository;
+import com.sprint.mission.discodeit.repository.file.FileMessageRepository;
+import com.sprint.mission.discodeit.repository.file.FileUserRepository;
 import com.sprint.mission.discodeit.service.ChannelService;
 import com.sprint.mission.discodeit.service.MessageService;
 import com.sprint.mission.discodeit.service.UserService;
-import com.sprint.mission.discodeit.service.jcf.JCFChannelService;
-import com.sprint.mission.discodeit.service.jcf.JCFMessageService;
-import com.sprint.mission.discodeit.service.jcf.JCFUserService;
-
-import java.util.List;
+import com.sprint.mission.discodeit.service.basic.BasicChannelService;
+import com.sprint.mission.discodeit.service.basic.BasicMessageService;
+import com.sprint.mission.discodeit.service.basic.BasicUserService;
 
 public class JavaApplication {
+    static User setupUser(UserService userService) {
+        userService.save(new User("woody", "010-0000-1111", "woody1234", UserStatus.ONLINE));
+        return userService.findAll().stream().filter(user -> user.getName().equals("woody")).findFirst().orElse(null);
+
+    }
+
+    static Channel setupChannel(ChannelService channelService) {
+        channelService.save(new Channel("채널1"));
+        return channelService.findAll().stream().filter(user -> user.getName().equals("채널1")).findFirst().orElse(null);
+
+    }
+
+    static void messageCreateTest(MessageService messageService, User author, Channel channel) {
+        System.out.println("메시지 생성");
+        messageService.save(new Message("메세지1", author.getId(), channel.getId()));
+    }
+
     public static void main(String[] args) {
+        // 레포지토리 초기화
+        UserRepository userRepository = new FileUserRepository();
+        ChannelRepository channelRepository = new FileChannelRepository();
+        MessageRepository messageRepository = new FileMessageRepository();
 
-        // User
-        System.out.println("=============== 사용자 ===============");
-        User aaron = new User("Aaron", "010-1111-1111", "aaron", UserStatus.ONLINE);
-        User baron = new User("Baron", "010-2222-2222", "baron", UserStatus.ONLINE);
-        User caron = new User("Caron", "010-3333-3333", "caron", UserStatus.AWAY);
+        // 서비스 초기화
+        UserService userService = new BasicUserService(userRepository);
+        ChannelService channelService = new BasicChannelService(channelRepository);
+        MessageService messageService = new BasicMessageService(messageRepository, userService, channelService);
 
-        // 사용자 저장
-//        UserService userService = new JCFUserService();
-        UserService userService = JCFUserService.getInstance();
-
-        try {
-            userService.save(aaron);
-            userService.save(baron);
-            userService.save(caron);
-
-            System.out.println("단건 조회");
-            System.out.println(userService.find(aaron.getId()));
-            System.out.println("다건 조회");
-            System.out.println(userService.findAll());
-            // 사용자 수정
-            User newAaron = userService.find(aaron.getId());
-            newAaron.update("Aaron_2", null, UserStatus.toUserStatus("자리비움"), "aarorong");
-            userService.update(newAaron.getId(), newAaron);
-            System.out.println(userService.find(newAaron.getId()));
-            // 사용자 삭제
-            User deleteUser = userService.find(caron.getId());
-            userService.delete(deleteUser.getId());
-
-            System.out.println("최종 조회");
-            System.out.println(userService.findAll());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-
-        // Channel
-        System.out.println("=============== 채널 ===============");
-        Channel channel1 = new Channel("코드_공유");
-        Channel channel2 = new Channel("모각코");
-        Channel channel3 = new Channel("소통");
-
-//        ChannelService channelService = new JCFChannelService();
-        ChannelService channelService = JCFChannelService.getInstance();
-
-        try {
-            // 채널 저장
-            channelService.save(channel1);
-            channelService.save(channel2);
-            channelService.save(channel3);
-            System.out.println("단건 조회");
-            System.out.println(channelService.find(channel1.getId()));
-            System.out.println("다건 조회");
-            System.out.println(channelService.findAll());
-            // 채널 수정
-            Channel newChannel1 = channelService.find(channel1.getId());
-            newChannel1.changeName("new_Channel1");
-            channelService.update(newChannel1.getId(), newChannel1);
-            System.out.println(channelService.find(newChannel1.getId()));
-            // 채널 삭제
-            Channel deleteChannel = channelService.find(channel3.getId());
-            channelService.delete(deleteChannel.getId());
-
-            System.out.println("최종 조회");
-            System.out.println(channelService.findAll());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-
-        System.out.println("=============== 메세지 ===============");
-        Message message1 = new Message("첫번째 메세지 입니다.", aaron.getId(), channel2.getId());
-        Message message2 = new Message("두번째 메세지 입니다.", baron.getId(), channel1.getId());
-        Message message3 = new Message("세번째 메세지 입니다.", baron.getId(), channel1.getId());
-        Message message4 = new Message("아론의 두번째 메세지 입니다.", aaron.getId(), channel1.getId());
-        Message message5 = new Message("아론의 세번째 메세지 입니다.", aaron.getId(), channel1.getId());
-
-
-        MessageService messageService = new JCFMessageService(userService, channelService);
-        try {
-            // 메세지 저장
-            messageService.save(message1);
-            messageService.save(message2);
-            messageService.save(message3);
-            messageService.save(message4);
-            messageService.save(message5);
-
-            System.out.println("단건 조회");
-            System.out.println(messageService.find(message1.getId()));
-            System.out.println("다건 조회");
-            System.out.println(messageService.findAll());
-            // 메세지 수정
-            Message newMessage1 = messageService.find(message1.getId());
-            newMessage1.changeMessage("변경 된 메세지 입니다.");
-            messageService.update(newMessage1.getId(), newMessage1);
-            System.out.println(messageService.find(newMessage1.getId()));
-            // 메세지 삭제
-            Message deleteMessage = messageService.find(message3.getId());
-            messageService.delete(deleteMessage.getId());
-
-            System.out.println("최종 조회");
-            System.out.println(messageService.findAll());
-
-            // 특정 유저 메세지
-            System.out.println("-------" + aaron.getName() + "님의 메세지만 조회합니다. -------");
-
-            List<Message> userMessage = messageService.findByUserId(aaron.getId());
-            userMessage.forEach(System.out::println);
-
-            // 특정 채널 메세지
-            System.out.println("------- 채널명 : [" + channel2.getName() + "] 메세지만 조회합니다. -------");
-
-            List<Message> channelMessage = messageService.findByChannelId(channel2.getId());
-            channelMessage.forEach(System.out::println);
-
-            // 특정 채널 + 회원 메세지
-            System.out.println("------- 채널명 : [" + channel1.getName() + "] - " + aaron.getName() + "님 메세지만 조회합니다. -------");
-
-            List<Message> channelByUserMessage = messageService.findByChannelIdAndUserId(aaron.getId(), channel1.getId());
-            channelByUserMessage.forEach(System.out::println);
-
-            //심화 검증 테스트
-//            Message message6 = new Message("네번째 메세지 입니다.", UUID.randomUUID(), channel1.getId());
-//            messageService.save(message6);
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-
+        // 셋업
+        User user = setupUser(userService);
+        Channel channel = setupChannel(channelService);
+        // 테스트
+        messageCreateTest(messageService, user, channel);
     }
 }
