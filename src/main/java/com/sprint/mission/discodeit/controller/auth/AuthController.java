e29316f6007096eedb755eb7f46ccb4c4809df85
EUNSOLY <dmsthf9596@gmail.com>
2026-08-20 09:51:02 +0900

    fix : 코드잇 API 요구사항 기준으로 변경(DTO, Service, Controller)

--- src/main/java/com/sprint/mission/discodeit/controller/auth/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/auth/AuthController.java
@@ -1,9 +1,13 @@
 package com.sprint.mission.discodeit.controller.auth;
 
-import com.sprint.mission.discodeit.dto.auth.LoginRequestDto;
+import com.sprint.mission.discodeit.dto.auth.LoginRequest;
 import com.sprint.mission.discodeit.entity.user.User;
 import com.sprint.mission.discodeit.service.auth.BaseAuthService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,13 +22,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @Tag(name = "Auth", description = "인증 API")
+@ApiResponse(
+        responseCode = "200",
+        description = "로그인 성공",
+        content = @Content(schema = @Schema(implementation = User.class))
+)
+@ApiResponse(
+        responseCode = "404",
+        description = "사용자를 찾을 수 없음",
+        content = @Content(schema = @Schema(implementation = String.class), examples = @ExampleObject("User with username {username} not found"))
+)
+@ApiResponse(
+        responseCode = "400",
+        description = "비밀번호가 일치하지 않음",
+        content = @Content(schema = @Schema(implementation = String.class), examples = @ExampleObject("Wrong password"))
+)
 public class AuthController {
     private final BaseAuthService authService;
 
     @Operation(summary = "로그인", description = "이메일과 비밀번호로 인증한다.")
     @RequestMapping(method = RequestMethod.POST, value = "/api/auth/login")
     public ResponseEntity<User> login(
-            @RequestBody LoginRequestDto request
+            @RequestBody LoginRequest request
     ) {
         User loginUser = authService.login(request);
         return ResponseEntity.status(HttpStatus.OK)
---
+++ src/main/java/com/sprint/mission/discodeit/service/auth/BaseAuthService.java
@@ -0,0 +1,30 @@
+package com.sprint.mission.discodeit.service.auth;
+
+import com.sprint.mission.discodeit.common.dto.CustomStatusCode;
+import com.sprint.mission.discodeit.common.exception.GlobalCustomException;
+import com.sprint.mission.discodeit.dto.auth.LoginRequest;
+import com.sprint.mission.discodeit.entity.user.User;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BaseAuthService implements AuthService {
+    private final UserRepository userRepository;
+
+    public User login(LoginRequest loginRequest) {
+        String username = loginRequest.username();
+        String password = loginRequest.password();
+
+        boolean hasDuplicateName = userRepository.existsByName(username);
+        boolean hasDuplicateEmail = userRepository.existsByEmail(password);
+
+        
+        if (!hasDuplicateName || !hasDuplicateEmail) {
+            throw new GlobalCustomException(CustomStatusCode.INVALID_CREDENTIALS);
+        }
+
+        return userRepository.findByUsername(username).orElseThrow(() -> new GlobalCustomException(CustomStatusCode.USER_NOT_FOUND));
+    }
+}
